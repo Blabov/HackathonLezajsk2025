8bb6116110f74cf427d71328b5ec1ce530a0d26e
Blabov <108542211+Blabov@users.noreply.github.com>
2025-03-14 20:42:34 +0100

    a
    a

--- scenes/belt.gd
+++ scenes/belt.gd
@@ -6,5 +6,5 @@ func _physics_process(_delta: float) -> void:
 	speed = GlobalVariables.speed
 	velocity.x = speed
 	if position.x >= 1976:
-		position.x = -3976
+		position.x = -3930
 	move_and_slide()
