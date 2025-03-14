be750faec9a06ddbd6aac89f8e3f03f60b463e4f
Blabov <108542211+Blabov@users.noreply.github.com>
2025-03-14 20:34:15 +0100

    a
    a

--- scripts/game.gd
+++ scripts/game.gd
@@ -18,8 +18,8 @@ func _on_bottle_timer_timeout() -> void:
 	$Bottles.add_child(bottle)
 	bottle.global_position = $BottlesSpawnPosition.global_position
 	if GlobalVariables.score >= 100:
-		$BottleTimer.start(1 - GlobalVariables.score / 10)
-	else: $BottleTimer.start(0.1)
+		$BottleTimer.start(0.1)
+	else: $BottleTimer.start(1 - GlobalVariables.score / 5)
 	
 
 func _on_delivery_place_body_entered(body: Node2D) -> void:
