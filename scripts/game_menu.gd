220cda42f0769eb603c9aea320e6ae5b3bb73ea2
Blabov <108542211+Blabov@users.noreply.github.com>
2025-03-14 18:15:45 +0100

    a
    a

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -3,3 +3,7 @@ extends Control
 #przyciski
 func _on_play_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
+
+
+func _on_quit_button_button_up() -> void:
+	get_tree().quit()
