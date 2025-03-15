b0357bb6e502dc63e049dfc133f9486993dc37d3
Blabov <108542211+Blabov@users.noreply.github.com>
2025-03-15 04:09:14 +0100

    aaaa

--- scripts/game_over_screen.gd
+++ scripts/game_over_screen.gd
@@ -7,3 +7,7 @@ func _on_restart_button_button_up() -> void:
 
 func _on_quit_button_button_up() -> void:
 	get_tree().quit()
+
+
+func _on_main_menu_button_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/game_menu.tscn")
